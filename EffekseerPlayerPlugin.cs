accf69b5c291e2158d33a36003119eec6cfd8703
trzr <trzr@users.noreply.github.com>
2018-04-29 21:35:45 +0900

    release 0.2.1

--- EffekseerPlayerPlugin.cs
+++ EffekseerPlayerPlugin.cs
@@ -13,7 +13,7 @@ using UnityEngine.SceneManagement;
 using UnityInjector;
 using UnityInjector.Attributes;
 
-[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyVersion("0.2.1")]
 namespace EffekseerPlayer {
 #if COM3D2    
     [PluginFilter("COM3D2x64"),
@@ -22,7 +22,7 @@ namespace EffekseerPlayer {
      PluginFilter("CM3D2OHx64"),
 #endif
      PluginName("EffekseerPlayerPlugin"),
-     PluginVersion("0.2.0")]
+     PluginVersion("0.2.1")]
     public class EffekseerPlayerPlugin : PluginBase {
         #region Variables
         private const EventModifiers MODIFIER_KEYS = EventModifiers.Shift | EventModifiers.Control | EventModifiers.Alt;
